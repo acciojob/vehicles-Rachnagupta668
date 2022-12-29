f4259fe3a9ab338f2e3d9e03b0c78b15b5de6cf8
Rachna <rachnakum0476@gmail.com>
2022-12-29 16:03:42 +0530

    Vechile code getters added

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -3,7 +3,13 @@ package com.driver;
 public class Boat implements WaterVehicle{
     String name;
     int capacity;
+Boat(){
 
+}
+Boat(String name,int capacity){
+    this.capacity = capacity;
+    this.name= name;
+}
 
     @Override
     public String getVehicleName() {
